5d8f428cbd8ae2ba52696de03dd6008ce82a8636
Eric Winkelman <e.winkelman@cablelabs.com>
2014-06-17 13:48:58 -0600

    Added check for bad file names

--- upload.php
+++ upload.php
@@ -6,17 +6,20 @@ header("Access-Control-Allow-Origin: web-platform.test");
 $data = "no data";
 
 if (isset($_POST["file"]) && isset($_POST["log"])) {
-   $file = $_POST["file"];
-   $data = $_POST["log"];
+  $file = $_POST["file"];
+  $data = $_POST["log"];
 
-   $fp = fopen("./logs/$file.log", "a");
-   if (!$fp) {
-     echo "Not able to open file: $file";
-     return;
-   }
+  if (preg_match('/^[0-9a-km-z]{6}$/', $file)) {
 
-   fwrite($fp, $data, strlen($data));
-   fclose($fp);
+    $fp = fopen("./logs/$file.log", "a");
+    if (!$fp) {
+      echo "Not able to open file: $file";
+      return;
+    }
+
+    fwrite($fp, $data, strlen($data));
+    fclose($fp);
+  }
 } else {
   echo "Missing: file or log parameter";
 }
