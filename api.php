7753e768310fbf25d911c6e82c79aa87413337e3
Jeremy Poulter <jeremy@bigjungle.net>
2015-11-05 15:21:11 +0000

    Dissable memory limitation so large results sets can be downloaded. Not an ideal solution but PHP Slim does not allow streaming results.

--- api.php
+++ api.php
@@ -9,6 +9,7 @@ require 'vendor/autoload.php';
 require 'session.php';
 
 error_reporting(E_ALL);
+ini_set('memory_limit', -1);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
